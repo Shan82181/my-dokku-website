dcba3a38576a698acd9dc0e5749868dc3ba08812
Shantanu Pal <shantanupal229@.com>
2025-03-17 18:30:53 +0530

    new changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Doccu Website',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
